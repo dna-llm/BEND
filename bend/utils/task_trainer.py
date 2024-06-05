ca311968b5374dd5c99dac3bf3d1447f3fde3e48
Hassan <69965978+hssn-20@users.noreply.github.com>
2024-06-05 18:57:48 +0100

    Update task_trainer.py

--- bend/utils/task_trainer.py
+++ bend/utils/task_trainer.py
@@ -21,7 +21,7 @@ def append_to_log(text):
     
     with open(log_file, 'a') as file:
         text = text.cpu().numpy()
-        file.write(text.tolist())
+        file.write(','.join(text.tolist()))
 
 
 class CrossEntropyLoss(nn.Module):
