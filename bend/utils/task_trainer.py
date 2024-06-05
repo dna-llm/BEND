f9d2971b6a5eedcb47b7b2892177bf2c454a40a2
Hassan <69965978+hssn-20@users.noreply.github.com>
2024-06-05 19:05:15 +0100

    Update task_trainer.py

--- bend/utils/task_trainer.py
+++ bend/utils/task_trainer.py
@@ -20,8 +20,7 @@ def append_to_log(text):
     log_file = 'test_1.txt'
     
     with open(log_file, 'a') as file:
-        text = text.detach().cpu().numpy().tostring()
-        print(text)
+        text = text.detach().cpu().numpy().astype('str') 
         file.write(','.join(text.tolist()))
 
 
