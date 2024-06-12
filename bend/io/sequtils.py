f291d09e2e27feed2a07fbd87343360b1fe24e5d
Hassan <69965978+hssn-20@users.noreply.github.com>
2024-06-12 14:15:41 +0100

    Update sequtils.py

--- bend/io/sequtils.py
+++ bend/io/sequtils.py
@@ -1,257 +1,103 @@
-"""
-sequtils.py
-===========
-Utilities for processing genome coordinate-based sequence data to embeddings.
-"""
+import os
 
-import h5py
-import numpy as np
+import hydra
 import pandas as pd
-import pysam
-import webdataset as wds
-from datasets import load_dataset
-from tqdm.auto import tqdm
+from bend.io import sequtils
+from omegaconf import DictConfig
 
-baseComplement = {"A": "T", "C": "G", "G": "C", "T": "A"}
+# CONFIG_PATH = "./conf/"  # "/kaggle/working/BEND/conf/"
 
 
-# def multi_hot(labels, num_labels):
-#     """
-#     Convert a numpy array to a one-hot encoded numpy array.
-
-#     Parameters
-#     ----------
-#     labels : list
-#         The labels that are true
-#     num_labels : int
-#         The number of potential labels.
-
-#     Returns
-#     -------
-#     numpy.ndarray
-#         A multi-hot encoded numpy array.
-#     """
-#     encoded = np.eye(num_labels, dtype=np.int64)[labels].sum(axis=0)
-
-#     # encoded = np.zeros((num_labels), dtype=np.int64)
-#     # for i, row in enumerate(labels):
-#     #    encoded[row] = 1
-#     return encoded
-
-def multi_hot(labels, num_labels):
+# load config
+@hydra.main(config_path="/kaggle/working/BEND/conf/", config_name="embed", version_base=None)
+def run_experiment(cfg: DictConfig) -> None:
     """
-    Convert a numpy array to a one-hot encoded numpy array.
-
+    Run a embedding of nucleotide sequences.
+    This function is called by hydra.
     Parameters
     ----------
-    labels : list
-        The labels that are true
-    num_labels : int
-        The number of potential labels.
-
-    Returns
-    -------
-    numpy.ndarray
-        A multi-hot encoded numpy array.
+    cfg : DictConfig
+        Hydra configuration object.
     """
-    encoded = np.zeros(num_labels, dtype=np.int64)
-    encoded[labels] = 1
-    return encoded
+    print("Embedding data for", cfg.task)
+    # read the bed file and get the splits :
+    if "splits" not in cfg or cfg.splits is None:
+        splits = sequtils.get_splits(cfg[cfg.task].bed)
+    else:
+        splits = cfg.splits
+    print("Embedding with", cfg.model)
+    # instatiante model
+    embedder = hydra.utils.instantiate(cfg[cfg.model])
+    for split in splits:
+        print(f"Embedding split: {split}")
+        output_dir = f"{cfg.data_dir}/{cfg.task}/{cfg.model}/"
+
+        os.makedirs(output_dir, exist_ok=True)
+
+        # embed in chunks
+        # get length of bed file and divide by chunk size, if a spcific chunk is not set
+        df = pd.read_csv(cfg[cfg.task].bed, sep="\t", low_memory=False)
+        df = df[df.iloc[:, -1] == split] if split is not None else df
+        possible_chunks = list(range(int(len(df) / cfg.chunk_size) + 1))
+        if cfg.chunk is None:
+            cfg.chunk = possible_chunks
+        cfg.chunk = [cfg.chunk] if isinstance(cfg.chunk, int) else cfg.chunk
+        # embed in chunks
+        for _n, chunk in enumerate(cfg.chunk):
+            if chunk not in possible_chunks:
+                print(f"{chunk} is not a valid chunk id. {split} chunk ids are {possible_chunks}")
+                continue
+            print(f"\t Embedding chunk {chunk} ({chunk + 1}/{len(possible_chunks)})")
+            sequtils.embed_from_bed(
+                **cfg[cfg.task],
+                embedder=embedder,
+                output_path=f"{output_dir}/{split}_{chunk}.tar.gz",
+                split=split,
+                chunk=chunk,
+                chunk_size=cfg.chunk_size,
+                upsample_embeddings=cfg[cfg.model]["upsample_embeddings"]
+                if "upsample_embeddings" in cfg[cfg.model]
+                else False,
+            )
 
 
-def reverse_complement(dna_string: str):
-    # """Returns the reverse-complement for a DNA string."""
+@hydra.main(config_path="/kaggle/working/BEND/conf/", config_name="embed", version_base=None)
+def run_experiment_hf(cfg: DictConfig) -> None:
     """
-    Returns the reverse-complement for a DNA string.
-
+    Run a embedding of nucleotide sequences.
+    This function is called by hydra.
     Parameters
     ----------
-    dna_string : str
-        DNA string to reverse-complement.
-
-    Returns
-    -------
-    str
-        Reverse-complement of the input DNA string.
+    cfg : DictConfig
+        Hydra configuration object.
     """
+    print("Embedding data for", cfg.task)
+    # read the bed file and get the splits :
 
-    complement = [baseComplement.get(base, "N") for base in dna_string]
-    reversed_complement = reversed(complement)
-    return "".join(list(reversed_complement))
+    print("Embedding with", cfg.model)
+    # instatiante model
+    embedder = hydra.utils.instantiate(cfg[cfg.model])
+    splits = ["train"]
+    repo_id = "DNA-LLM/gene_finding_benchmark_v1"
+    for split in splits:
+        print(f"Embedding split: {split}")
+        output_dir = f"{cfg.data_dir}/{cfg.task}/{cfg.model}/"
 
+        os.makedirs(output_dir, exist_ok=True)
 
-# %%
-class Fasta:
-    """Class for fetching sequences from a reference genome fasta file."""
-
-    def __init__(self, fasta) -> None:
-        """
-        Initialize a Fasta object for fetching sequences from a reference genome fasta file.
-
-        Parameters
-        ----------
-        fasta : str
-            Path to a reference genome fasta file.
-        """
-
-        self._fasta = pysam.FastaFile(fasta)
-
-    def fetch(self, chrom: str, start: int, end: int, strand: str = "+", flank: int = 0) -> str:
-        """
-        Fetch a sequence from the reference genome fasta file.
-
-        Parameters
-        ----------
-        chrom : str
-            Chromosome name.
-        start : int
-            Start coordinate.
-        end : int
-            End coordinate.
-        strand : str, optional
-            Strand. The default is '+'.
-            If strand is '-', the sequence will be reverse-complemented before returning.
-        flank : int, optional
-            Number of bases to add to the start and end coordinates. The default is 0.
-        Returns
-        -------
-        str
-            Sequence from the reference genome fasta file.
-        """
-        sequence = self._fasta.fetch(str(chrom), start - flank, end + flank).upper()
-
-        if strand == "+":
-            pass
-        elif strand == "-":
-            sequence = "".join(reverse_complement(sequence))
-        else:
-            raise ValueError(f"Unknown strand: {strand}")
-
-        return sequence
+        sequtils.embed_from_hf(
+            **cfg[cfg.task],
+            repo_id=repo_id,
+            embedder=embedder,
+            output_path=f"{output_dir}/{split}.tar.gz",
+            split=split,
+            upsample_embeddings=cfg[cfg.model]["upsample_embeddings"]
+            if "upsample_embeddings" in cfg[cfg.model]
+            else False,
+        )
 
 
-def embed_from_bed(
-    bed,
-    reference_fasta,
-    embedder,
-    output_path,
-    hdf5_file=None,
-    chunk_size=None,
-    chunk=None,
-    upsample_embeddings=False,
-    read_strand=False,
-    label_column_idx=6,
-    label_depth=None,
-    split=None,
-    flank=0,
-):
-    fasta = Fasta(reference_fasta)
-    f = pd.read_csv(bed, header="infer", sep="\t", low_memory=False)
-    # open hdf5 file
-    hdf5_file = h5py.File(hdf5_file, mode="r")["labels"] if hdf5_file else None
-    if split:
-        mask = f.iloc[:, -1] == split
-        f = f[mask]
-        if hdf5_file is not None:
-            hdf5_file = hdf5_file[mask.to_numpy()]  # mask the labels
+if __name__ == "__main__":
+    print("Run Embedding")
 
-    label_column_idx = f.columns.get_loc("label") if "label" in f.columns else label_column_idx
-    strand_column_idx = f.columns.get_loc("strand") if "strand" in f.columns else 3
-
-    if chunk is not None:
-        # check if chunk is valid
-        if chunk * chunk_size > len(f):
-            raise ValueError(
-                f"Requested chunk {chunk}, but chunk ids range from 0-{int(len(f) / chunk_size)}"
-            )
-        f = f[chunk * chunk_size : (chunk + 1) * chunk_size].reset_index(drop=True)
-
-    buffer_inputs = []
-    buffer_labels = []
-    start_offset = chunk * chunk_size
-    buffer_size = 5000
-
-    sink = wds.TarWriter(output_path, compress=True)
-    for n, line in tqdm(f.iterrows(), total=len(f), desc="Embedding sequences"):
-        # get bed row
-        if read_strand:
-            chrom, start, end, strand = (
-                line.iloc[0],
-                int(line.iloc[1]),
-                int(line.iloc[2]),
-                line.iloc[strand_column_idx],
-            )
-        else:
-            chrom, start, end, strand = line.iloc[0], int(line.iloc[1]), int(line.iloc[2]), "+"
-        if hdf5_file is not None:
-            labels = hdf5_file[n + start_offset]
-        else:
-            labels = line.iloc[label_column_idx]
-            labels = (
-                list(map(int, labels.split(","))) if isinstance(labels, str) else []
-            )  # if no label for sample
-            labels = multi_hot(labels, label_depth)
-        # get sequence
-        sequence = fasta.fetch(chrom, start, end, strand=strand, flank=flank)  # categorical labels
-        # embed sequence
-        sequence_embed = embedder(sequence, upsample_embeddings=upsample_embeddings)
-        if sequence_embed.shape[1] != len(sequence):
-            print(
-                f"Embedding length does not match sequence length ({sequence_embed.shape[1]} != {len(sequence)} : {n} {chrom}:{start}-{end}{strand})"
-            )
-            print(n, chrom, start, end, strand)
-            continue
-        sink.write({
-            "__key__": f"sample_{n + start_offset}",
-            "input.npy": sequence_embed,
-            "output.npy": labels,
-        })
-
-    sink.close()
-
-
-def embed_from_hf(
-    bed,
-    repo_id,
-    reference_fasta,
-    embedder,
-    output_path,
-    hdf5_file=None,
-    chunk_size=None,
-    chunk: int = None,
-    upsample_embeddings=False,
-    read_strand=False,
-    label_column_idx=6,
-    label_depth=9,
-    split=None,
-    flank=0,
-):
-    ds = load_dataset(repo_id)
-    ds = ds[split]
-    df = pd.DataFrame(ds)
-    df = df.iloc[:10].copy()
-    sink = wds.TarWriter(output_path, compress=True)
-    for n, line in tqdm(df.iterrows(), total=len(df), desc="Embedding sequences"):
-        sequence = line["seq"]
-        sequence_embed = embedder(sequence, upsample_embeddings=upsample_embeddings)
-        labelss = [multi_hot(item, label_depth) for item in line['labels']]
-        labelss = np.array(labelss)
-        if sequence_embed.shape[1] != len(line["seq"]):
-            print(
-                f"Embedding length does not match sequence length ({sequence_embed.shape[1]} != {len(sequence)})"
-            )
-            continue
-        sink.write({
-            "__key__": f"sample_{n}",
-            "input.npy": sequence_embed,
-            "output.npy": labelss
-        })
-
-    sink.close()
-
-
-def get_splits(bed):
-    # header = 'infer' if has_header(bed) else None
-    f = pd.read_csv(bed, header="infer", sep="\t")
-    splits = f.iloc[:, -1].unique().tolist()  # splits should be in last column
-    return splits
+    run_experiment_hf()
